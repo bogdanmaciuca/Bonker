24c714f97d92dc3c68805dd30eadcdbfd7f8f9be
bogdanmaciuca <bogdanmaciuca0621@gmail.com>
2026-06-04 15:28:33 +0300

    sql schema

---
+++ app/src/main/resources/schema.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS transaction;
+DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS clients;
+
+CREATE TABLE client (
+    id_number  TEXT PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name  TEXT NOT NULL
+);
+
+CREATE TABLE account (
+    iban TEXT PRIMARY KEY,
+    client_id TEXT NOT NULL,
+    currency  TEXT NOT NULL,
+    balance   REAL NOT NULL DEFAULT 0,
+    type      TEXT NOT NULL,
+    interest  REAL NOT NULL DEFAULT 0,
+    FOREIGN KEY (client_id) REFERENCES client(id_number)
+);
+
+CREATE TABLE card (
+    number       TEXT PRIMARY KEY,
+    exp_date     TEXT NOT NULL,
+    cvv          TEXT NOT NULL,
+    account_iban TEXT NOT NULL,
+    FOREIGN KEY (account_iban) REFERENCES account(iban)
+);
+
+CREATE TABLE transaction (
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    account_iban TEXT NOT NULL,
+    type         TEXT NOT NULL,
+    amount       REAL NOT NULL,
+    currency     TEXT NOT NULL,
+    timestamp    TEXT NOT NULL,
+    FOREIGN KEY (account_iban) REFERENCES account(iban)
+);
